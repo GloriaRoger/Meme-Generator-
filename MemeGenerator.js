8a903005375b3aafaa73b8762421768fd62a97cf
GloriaRoger <roger.gloria77@gmail.com>
2023-07-02 18:23:08 -0400

    working on positioning fix. js working

--- MemeGenerator.js
+++ MemeGenerator.js
@@ -8,15 +8,16 @@ document.getElementById("memeGenerator").addEventListener('submit', function(e) 
     const newMeme = document.createElement('div');
     newMeme.id = "newMeme";
     newMeme.classList.add('newMeme');
-    
+        
     const newMemeBack = document.createElement('img');
     newMemeBack.src = imageUrl;
-    newMemeBack.id ="newMemeBack"
+    newMemeBack.className ="newMemeBack"
     const newMemeTopText = document.createElement("h2");
+    newMemeTopText.className ="topText"
     newMemeTopText.innerText = topText;
     const newMemeBottomText = document.createElement("h2");
     newMemeBottomText.innerText = bottomText;  
-   
+    newMemeBottomText.className ="bottomText"
     
     newMeme.appendChild(newMemeTopText);
     newMeme.appendChild(newMemeBottomText);
