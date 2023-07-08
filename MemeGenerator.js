c170e2a5a1155b514d7c2cb0011b5d796a409a1e
GloriaRoger <roger.gloria77@gmail.com>
2023-07-08 19:17:20 -0400

    remove button working

--- MemeGenerator.js
+++ MemeGenerator.js
@@ -1,35 +1,47 @@
 document.getElementById("memeGenerator").addEventListener('submit', function(e) {
         e.preventDefault();
-
+// creating Variables from the Meme Generator Form input values by grabbing them using 'getElementbyId' 
     const imageUrl = document.getElementById("imageUrl").value;
     const topText = document.getElementById("topText").value;
     const bottomText = document.getElementById("bottomText").value;
 
+    //Create Div container for the Generated Meme to append to 
     const newMeme = document.createElement('div');
     newMeme.classList.add('newMeme');
-        
+    //Create "Generated Meme" Image variable from the image link input variable established above  
     const newMemeBack = document.createElement('img');
     newMemeBack.src = imageUrl;
-    newMemeBack.className ="newMemeBack";
+    newMemeBack.classList.add("newMemeBack");
+    //Create "Generated Meme" Top Text variable from the Top Text input variable established above  
     const newMemeTopText = document.createElement("p");
-    newMemeTopText.className ="topText";
+    newMemeTopText.classList.add("topText");
     newMemeTopText.innerText = topText;
+    newMeme.classList.add('newMeme');
+    //Create "Generated Meme" Bottom Text variable from the Bottom Text input variable established above 
     const newMemeBottomText = document.createElement("p");
     newMemeBottomText.innerText = bottomText;  
-    newMemeBottomText.className ="bottomText";
-    
+    newMemeBottomText.classList.add("bottomText");
+    //create "remove" button and append it to "newMeme" div. add event listenere to remove the "newMeme" from the "Memes" div established in html body
+    const removeButton = document.createElement('button');
+    removeButton.classList.add("removeButton");
+    removeButton.id ="removeButton";
+    removeButton.innerText = "Remove";
+    removeButton.addEventListener('click', function(e) {
+        this.parentElement.remove();
+    });
+    //append all elements to dive "newMeme" to create the "Generated Meme"
     newMeme.appendChild(newMemeBottomText);
     newMeme.appendChild(newMemeTopText);
     newMeme.appendChild(newMemeBack);
+    newMeme.appendChild(removeButton);
 
-    const removeButton = document.createElement('button');
-    removeButton.className ="removeButton";
-    removeButton.addEventListener('click', function(e) {
-        e.defaultPrevented
-
-    }
-    
+    //append the "newMeme" div to the established "memes" div in the body to view the NewMeme in the body
     document.getElementById('Memes').appendChild(newMeme);
     memeGenerator.reset();
 
+
           });
+          
+         
+          
+            
